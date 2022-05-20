6487b18dfc1cbe49cc6324ea57df3eee0013daa6
Nicolás Klopstock <n.klopstock@uniandes.edu.co>
2022-05-20 07:10:17 -0500

    35%

--- src/app/articulo/articulo-detail/articulo-detail.component.ts
+++ src/app/articulo/articulo-detail/articulo-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Articulo } from '../articulo';
 
 @Component({
   selector: 'app-articulo-detail',
--- src/app/articulo/articulo-list/articulo-list.component.ts
+++ src/app/articulo/articulo-list/articulo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Articulo } from '../articulo';
+import { ArticuloService } from '../articulo.service';
 
 @Component({
   selector: 'app-articulo-list',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticuloListComponent implements OnInit {
 
-  constructor() { }
+  articulos: Array<Articulo> = [];
+  selectedArticulo!: Articulo;
+  isSelected: boolean = false;
+  precioPromedio: number = 0;
+
+  constructor(private articuloService: ArticuloService) { }
+
+  getArticulos(): void {
+    this.articuloService.getArticulos().subscribe( articulos => {
+      this.articulos = articulos;
+      this.calcularPrecioPromedio(this.articulos);
+    });
+  }
+
+  calcularPrecioPromedio(articulos: Array<Articulo>): void {
+    let precioTotal: number = 0;
+    for (let i = 0; i < articulos.length; i++) {
+      precioTotal += articulos[i].price;
+    }
+    this.precioPromedio = precioTotal / articulos.length;
+  }
+
+  onSelected(articulo: Articulo): void {
+    this.selectedArticulo = articulo;
+    this.isSelected = true;
+  }
 
   ngOnInit() {
+    this.getArticulos();
   }
 
 }
